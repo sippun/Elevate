28325c7d01b821a354502a68d1aaadfa7df4e789
krosengrant <krosengr@ucsc.edu>
2017-11-05 17:41:42 -0800

    Habit reminder notifications

---
+++ app/src/main/java/com/example/android/elevate/NotificationReceiver.java
@@ -0,0 +1,61 @@
+package com.example.android.elevate;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+
+
+/**
+ * Created by katierosengrant on 10/29/17.
+ *
+ * Creates the habit reminder notification.
+ * Change notification text/appearance here.
+ */
+
+class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Notification built here
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int NOTIFICATION_ID = 101;
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        String CHANNEL_ID = "my_channel_01";
+        CharSequence name = "habit reminder channel name";
+        String description = "habit reminder channel description";
+
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(android.R.color.transparent) /* required */
+                        .setContentTitle("Elevate")
+                        .setContentText("Have you completed your daily habits?");
+
+        Intent resultIntent = new Intent(context, RepeatingActivity.class);
+
+        // This ensures that navigating backward from the Activity leads out of
+        // your app to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(RepeatingActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        // pass the Notification object to the system
+        notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+}
